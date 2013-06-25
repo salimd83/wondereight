272e3cce6e6c882aa96999c5334e651b62e72cd7
Salim Dirani <salimdirani@gmail.com>
2013-06-25 09:49:02 +0300

    fix error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  unless Rails.application.config.consider_all_requests_local
+  # unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: lambda { |exception| render_error 500, exception }
     rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
-  end
+  # end
 
   private
 
